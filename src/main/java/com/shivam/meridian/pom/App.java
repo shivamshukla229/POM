022189531df735119f94bcb759253b4750e4565e
shivamshukla <ershivam.iimt@gmail.com>
2018-10-17 02:13:36 +0530

    Framework Commited

---
+++ src/main/java/com/shivam/meridian/pom/App.java
@@ -0,0 +1,13 @@
+package com.shivam.meridian.pom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
